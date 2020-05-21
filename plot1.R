944cb1b4618fc9aac44adbfeef87c764ec80c0d4
Binary-Poha <65278708+Binary-Poha@users.noreply.github.com>
2020-05-21 13:25:48 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,5 @@
+hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+
+#saving graph in png format
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
